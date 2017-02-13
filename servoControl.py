0beefe143b4e081a6f4c4d248b844fce8d56851d
Stefan268 <sstanojevic268@gmail.com>
2017-02-13 13:16:55 +0100

    Update servoControl.py

--- servoControl.py
+++ servoControl.py
@@ -3,39 +3,29 @@ import GPIO.PWM as PWM
 import time
 from lidar_lite import Lidar_Lite
 
-global distances[]
-
-connected = (-1)
-lidar = Lidar_Lite()
-while (connected == -1):
-    connected = lidar.connect(1)
-
-def getDist(self):
-    return self.lidar.getDistance()
-
-def setup():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(25, GPIO.OUT)
-    
-    servo = PWM(18, 50)
-    servo.start(0)
-    
-def loop():
-    while True:
-        for duty in range(11):
-            servo.ChangeDutyCycle(duty * 10)
-            time.sleep(1)#pause and wait for movement
-            distances[duty] = getDist()
-        sendData()
-        for duty in range(10,-1,-1):
-            servo.ChangeDutyCycle(duty * 10)
-            time.sleep(1)#pause and wait for movement
-            distances[duty] = getDist()
-        sendData()
-
-def sendData():
-    #send data to socket
-setup()
-loop()
 
 
+class lidarControl:
+
+    def setup(self, resolution):
+        self.lidar = Lidar_Lite()
+        self.lidar.connect(1)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(18, GPIO.OUT)
+        
+        self.servo = PWM(18, 50)
+        self.servo.start(0)
+        self.resolution = resolution
+        
+    def sweepRight(self):
+            for duty in range(self.resolution):
+                self.servo.ChangeDutyCycle(duty * 100/(self.resolution - 1)
+                time.sleep(1)#pause and wait for movement
+                self.distances[duty] = self.lidar.getDistance()
+            return distances
+    def sweepLeft(self):        
+            for duty in range((self.resolution-1),-1,-1):
+                self.servo.ChangeDutyCycle(duty * 100/(self.resolution - 1))
+                time.sleep(1)#pause and wait for movement
+                self.distances[duty] = self.lidar.getDistance()
+            return distances
